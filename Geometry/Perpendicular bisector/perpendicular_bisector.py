a4f380c23c91466883130d8a6762c505c1d0fea6
narek-mkrtumyan <81069641+narek-mkrtumyan@users.noreply.github.com>
2021-11-15 00:36:58 +0400

    edited perpendicular bisector

--- Geometry/Perpendicular bisector/perpendicular_bisector.py
+++ Geometry/Perpendicular bisector/perpendicular_bisector.py
@@ -3,8 +3,10 @@ import numpy as np
 from GeometryFunctions import *
 
 
-class PerpBisect(Scene):
+class PerpBisect(MovingCameraScene):
     def construct(self):
+
+        # self.camera.frame.save_state()
         
         # Points A and B, segment AB and midpoint M
         a = np.array([-1.5, -1, 0])
@@ -13,7 +15,7 @@ class PerpBisect(Scene):
         B = Dot(b)
         label_A = LabelPoint(A, 'A')
         label_B = LabelPoint(B, 'B', DR*0.5)
-        AB = Line(a, b)
+        AB = Line(A, B)
         M = Dot(AB.get_midpoint())
         label_M = LabelPoint(M, 'M', DOWN*0.75)
 
@@ -23,45 +25,85 @@ class PerpBisect(Scene):
         C = always_redraw(lambda: Dot([c_x.get_value(), c_y.get_value(), 0]))
         label_C = always_redraw(lambda: LabelPoint(C, 'C', UP*0.75))
 
-        # Lines CA and CB with color using ValueTracker
+        # Lines CA CB CM with color using ValueTracker
         color_frac = ValueTracker(0)
-        CA = always_redraw(lambda: Line(C.get_center(), a,
+        CA = always_redraw(lambda: Line(C.get_center(), A,
             color=rgb_to_hex(hex_to_rgb(RED)*(1-color_frac.get_value()) + hex_to_rgb(ORANGE)*color_frac.get_value())))
-        CB = always_redraw(lambda: Line(C.get_center(), b,
+        CB = always_redraw(lambda: Line(C.get_center(), B,
             color=rgb_to_hex(hex_to_rgb(GREEN)*(1-color_frac.get_value()) + hex_to_rgb(ORANGE)*color_frac.get_value())))
 
-        def move_C_to_perp_bisect_and_change_colors():
-            self.play(c_x.animate(rate_func=linear).set_value((a[0] + b[0]) / 2), c_y.animate().set_value(2), 
-                color_frac.animate(rate_func=linear).set_value(1), run_time=5)
-        
-        # Equality signs for CA and CB with 1 small line
-        equality_sign_CA = always_redraw(lambda: SegmentsEqualitySign_1(CA))
-        equality_sign_CB = always_redraw(lambda: SegmentsEqualitySign_1(CB))
         # Equality signs for CA and CB with 2 small lines
         equality_sign_AM = SegmentsEqualitySign_2(Line(A, M))
         equality_sign_BM = SegmentsEqualitySign_2(Line(B, M))
 
-        # test animations
+
+
+        # DRAWING ANIMATION BLOCKS
+
+        def move_C_to_PB_and_change_colors(run_time):
+            # Function for animationg moving C to the Perpendicular bisector and changing colos of CA and CB
+            self.play(c_x.animate(rate_func=linear).set_value((a[0] + b[0]) / 2), c_y.animate().set_value(2.5), 
+                color_frac.animate(rate_func=linear).set_value(1), run_time=run_time)
+
+        def move_C_down_and_up(run_time=2.5):
+            # Function for moving C down then up
+            self.play(c_y.animate(rate_func=linear).set_value(1), run_time=run_time)
+            self.wait(1)
+            self.play(c_y.animate(rate_func=linear).set_value(3), run_time=run_time)
+
+        def Create_ABC_MoveC_To_PB(moving_run_time=5):
+            # ANIMATION: Create A, B, C, CA, CB and move C to the perpendicular bisector of AB
+            self.play(Create(A), Create(label_A))
+            self.play(Create(B), Create(label_B))
+            self.wait(0.3)
+            self.play(Create(C), Create(label_C))
+            self.play(Create(CA), Create(CB))
+            move_C_to_PB_and_change_colors(run_time=moving_run_time)
+            self.add(A, B, C) # To put the points on top of the lines
+
+        def Add_M_MC():
+            # ANIMATION: Put equality signs, Create AB, M, CM
+            self.play(Create(equality_sign_CA))
+            self.play(Create(equality_sign_CB))
+            self.play(Create(AB))
+            self.play(Create(M), Create(label_M))
+            self.play(Create(equality_sign_AM))
+            self.play(Create(equality_sign_BM))
+            self.play(Create(CM))
+            self.add(C, M) # To put the points on top of the lines
+            self.play(FadeIn(ACM),FadeIn(BCM))
+            self.wait(0.5)
+            self.play(self.camera.frame.animate.shift(3*RIGHT))
+
+
+        # ANIMATIONS FOR DRAWING
         self.wait(0.5)
-        self.add(A, B, C, CA, CB, AB, M, label_C, label_A, label_B, equality_sign_AM, equality_sign_BM, label_M)
-        move_C_to_perp_bisect_and_change_colors()
-        self.add(equality_sign_CB, equality_sign_CA)
-        # self.play(c_y.animate().set_value(a[1]))
-        self.wait()
+
+        Create_ABC_MoveC_To_PB(moving_run_time=5)
+        
+        # NOT ANIMATION: init new mobjects dependent on ValueTracker value
+        # Equality signs for CA and CB with 1 small line and segment CM
+        equality_sign_CA = always_redraw(lambda: SegmentsEqualitySign_1(CA))
+        equality_sign_CB = always_redraw(lambda: SegmentsEqualitySign_1(CB))
+        CM = always_redraw(lambda: Line(C.get_center(), M.get_center(), color=GREEN))
+        # Color ACM and BCM triangles
+        ACM = Polygon(A.get_center(), C.get_center(), M.get_center(), fill_opacity=0.3).set_fill(ORANGE).set_stroke(width=0)
+        BCM = Polygon(B.get_center(), C.get_center(), M.get_center(), fill_opacity=0.3).set_fill(GREEN).set_stroke(width=0)
+
+        Add_M_MC()
+
+        self.wait(1)
 
 
 
-        # # FINAL ANIMATIONS
-        # self.wait(0.5)
 
-        # self.play(Create(A), Create(label_A))
-        # self.play(Create(B), Create(label_B))
-        # self.wait(0.3)
-        # self.play(Create(C), Create(label_C))
-        # self.play(Create(CA), Create(CB))
-        # move_C_to_perp_bisect_and_change_colors()
+        # SOLUTION
 
-        # self.wait(0.5)
+        fc = self.camera.frame_center
+
+        solution = Tex()
+
+
         
 
 class test(Scene):
