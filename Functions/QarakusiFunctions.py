ebec127c61d480197cc3862289f21cbe65329454
Narek Mkrtmuyan <narekmk2001@gmail.com>
2022-02-06 12:21:15 +0400

    adds cut_with_scissors()

--- Functions/QarakusiFunctions.py
+++ Functions/QarakusiFunctions.py
@@ -1,10 +1,12 @@
 from manim import *
 import numpy as np
-
 import sys
-sys.path.append("../../")       # relative path to the root of the repository
+sys.path.append("../../../")
+sys.path.append("../../")
 sys.path.append("../")
 from Functions.GeometryFunctions.GeometryFunctions import *
+
+
 
 left_bound = Line([-3.5, 5, 0], [-3.5, -5, 0])
 right_bound = Line([3.5, 5, 0], [3.5, -5, 0])
@@ -13,8 +15,38 @@ bounds = VGroup(left_bound, right_bound)
 armenian_tex_template = TexTemplate()
 armenian_tex_template.add_to_preamble(r"\usepackage{armtex}")
 
+# ANIMAL SVGs or PNGs
+# RABBIT = SVGMobject('/SVG_PNG_files/rabbit.svg').set_color(WHITE).scale(0.55)
+# PIGEON = SVGMobject('/SVG_PNG_files/pigeon.svg').set_color(WHITE).scale(0.5)
+
+# CAGE_SQUARE = SVGMobject('Functions/SVG_PNG_files/cage_square.svg').set_color(GOLD).scale(1.5)
+# CAGE_BIRD = ImageMobject('../Functions/SVG_PNG_files/cage_bird.png').set_color(GOLD).scale(0.5)
+
+# SMOOTH_THINKING_BUBBLE_LEFT_1 = ImageMobject('../Functions/SVG_PNG_files/smooth_thinking_bubble_left_1.png').set_color(WHITE).scale(0.5)
+# SMOOTH_THINKING_BUBBLE_RIGHT_1 = ImageMobject('../Functions/SVG_PNG_files/smooth_thinking_bubble_right_1.png').set_color(WHITE).scale(0.5)
+
+# SMOOTH_THINKING_BUBBLE_LEFT_2 = ImageMobject('../Functions/SVG_PNG_files/smooth_thinking_bubble_left_2.png').set_color(WHITE).scale(0.5)
+# SMOOTH_THINKING_BUBBLE_RIGHT_2 = ImageMobject('../Functions/SVG_PNG_files/smooth_thinking_bubble_right_2.png').set_color(WHITE).scale(0.5)
+
+# THINKING_BUBBLE_LEFT_1 = ImageMobject('../Functions/SVG_PNG_files/thinking_bubble_left_1.png').set_color(WHITE).scale(0.5)
+# THINKING_BUBBLE_RIGHT_1 = ImageMobject('../Functions/SVG_PNG_files/thinking_bubble_right_1.png').set_color(WHITE).scale(0.5)
+
+# THINKING_BUBBLE_LEFT_2 = ImageMobject('../Functions/SVG_PNG_files/thinking_bubble_left_2.png').set_color(WHITE).scale(0.6)
+# THINKING_BUBBLE_RIGHT_2 = ImageMobject('../Functions/SVG_PNG_files/thinking_bubble_right_2.png').set_color(WHITE).scale(0.6)
+
+
+# open_scissors = SVGMobject('../Functions/SVG_PNG_files/open_scissors.svg')
+# open_scissors.set_color(WHITE).rotate(PI/10).scale(0.5)
+# closed_scissors = ImageMobject('SVG_PNG_files/closed_scissors.png')
+# closed_scissors.set_color(WHITE).scale(0.13).shift(0.1*DOWN).rotate(PI/5)
+
+
+
+# Մասերով խնդիրների համար ֆունկցիաներ
 
 DEFAULT_ENDMARK_LENGTH = 0.4
+
+
 
 def Endmark(length=DEFAULT_ENDMARK_LENGTH, color=WHITE):
     return Line([0, length/2, 0], [0, -length/2, 0], color=color)
@@ -46,6 +78,57 @@ def Segment(start=[-1, 0, 0], end=[1, 0, 0], mathtex=False, position=UP,
         mathtex = MathTex('.', font_size=1)
         mathtex.next_to(line.get_center(), position * 2)
         return VGroup(line, mathtex)
+
+
+def cut_with_scissors(self, open_scissors, closed_scissors, cut_coordinate=ORIGIN):
+    '''
+        At first, you have to init this variables - 
+
+        open_scissors = SVGMobject('ROOT/Functions/SVG_PNG_files/open_scissors.svg')
+
+        open_scissors.set_color(WHITE).rotate(PI/10).scale(0.5)
+
+        closed_scissors = ImageMobject('ROOT/Functions/SVG_PNG_files/closed_scissors.png')
+        
+        closed_scissors.set_color(WHITE).scale(0.13).shift(0.1*DOWN).rotate(PI/5)
+    '''
+    cut_coordinate = np.array(cut_coordinate) - np.array([0.2, 0.4, 0])
+
+    self.play(FadeIn(open_scissors.move_to(cut_coordinate - np.array([0, 0.5, 0]))))
+    self.wait(0.25)
+    self.play(open_scissors.animate().move_to(cut_coordinate))
+    self.wait(0.25)
+    self.remove(open_scissors)
+    self.add(closed_scissors.move_to(cut_coordinate - np.array([0.1, 0.1, 0])))
+    self.wait(0.5)
+    self.remove(closed_scissors)
+    self.add(open_scissors)
+    self.wait(0.25)
+    self.play(open_scissors.animate().move_to(cut_coordinate - np.array([0, 0.5, 0])))
+    self.wait(0.25)
+    self.play(FadeOut(open_scissors))
+
+
+def MergeSegments(segments, color=WHITE, endmark_color=WHITE, stroke_width=DEFAULT_STROKE_WIDTH,
+    sum_mathtexs=Dot(radius=0), sum_position=UP
+):
+    combined_segment = Segment(
+            segments[0][0][0].get_start_and_end()[0], segments[-1][0][0].get_start_and_end()[1], 
+            mathtex=sum_mathtexs, position=sum_position, color=color, 
+            endmark_color=endmark_color, stroke_width=stroke_width
+        )
+    return combined_segment
+
+
+def PlayMergeSegments(self, segments, combined_segment):
+
+    self.add(combined_segment[0])
+    self.play(FadeOut(*[seg[0] for seg in segments]))
+    self.play(ReplacementTransform(VGroup(*[seg[1] for seg in segments]), combined_segment[1]))
+
+
+
+
 
 
 def MultiplySegment(self, segment, factor=2, direction=RIGHT, 
@@ -80,22 +163,15 @@ def MultiplySegment(self, segment, factor=2, direction=RIGHT,
         )
     
     if merge_segments:
-        combined_segment = Segment(
-                segments[0][0][0].get_start_and_end()[0], segments[-1][0][0].get_start_and_end()[1], 
-                mathtex=sum_mathtexs, position=sum_position, color=segment[0][0].get_color(), 
-                endmark_color=segment[0][1].get_color(), stroke_width=segments[0][0][0].get_stroke_width()
-            )
-        
-        self.add(combined_segment[0])
-        self.play(FadeOut(*[seg[0] for seg in segments]))
-        self.play(ReplacementTransform(VGroup(*[seg[1] for seg in segments]), combined_segment[1]))
+        combined_segment = MergeSegments(segments, sum_mathtexs=sum_mathtexs, sum_position=sum_position)
+        PlayMergeSegments(self, segments, combined_segment)
         segments = combined_segment
 
-    return segments
+    return segments    
 
 
-def MultiplySegmentRotating(self, segment, factor=2, direction=RIGHT, color = BLUE, 
-    merge_segments=True, sum_mathtexs=False, sum_position=UP, run_time=3
+def MultiplySegmentRotating(self, segment, factor=2, direction=RIGHT, color=GREEN, 
+    merge_segments=False, sum_mathtexs=False, sum_position=UP, run_time=3
 ):
     '''
             Expends given segment by copy pasting it in a sequence by rotating
@@ -156,19 +232,22 @@ def MultiplySegmentRotating(self, segment, factor=2, direction=RIGHT, color = BL
 
 class test(Scene):
     def construct(self):
-        txt = MathTex('2', font_size=50)
-        sum = MathTex('8', font_size=50)
-        self.add(txt)
-        segment = Segment([-3, 0, 0], [-2, 0, 0], txt, position=DOWN)
 
-        # segment= Segment([-4, 0, 0], [-3, 0, 0], mathtex=MathTex('2', font_size=50), color=GREEN, endmark_color=ORANGE)
+        def play_number_counter(n=1, m=2, center=ORIGIN, color=WHITE):
+            numbers = VGroup(*[MathTex(rf'{i}', color=color) for i in range(n, m+1)])
+            numbers.move_to(center).set_opacity(0).shift(0.5*UP)
+            numbers[0].shift(0.5*DOWN)
 
-        self.add(segment)
+            self.play(numbers[0].animate().set_opacity(1))
 
-        # segments = MultiplySegment(self, segment, 5, merge_segments=True, sum_mathtexs=MathTex(r'10', font_size=50), run_time=5)
+            for i in range(len(numbers) - 1):
+                self.play(
+                numbers[i].animate().shift(0.5*DOWN).set_opacity(0),
+                numbers[i+1].animate().shift(0.5*DOWN).set_opacity(1)
+            )
 
-        MultiplySegmentRotating(self, segment, 4, RIGHT, sum_mathtexs=sum, sum_position=DOWN)
-        self.wait(1)
+            return numbers[-1]
+            
 
-        
+        last_number = play_number_counter(1, 5)
 
