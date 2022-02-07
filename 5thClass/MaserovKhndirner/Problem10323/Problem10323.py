90b6c372c7a32afc5184a21614d491af3a58b4bd
Narek Mkrtmuyan <narekmk2001@gmail.com>
2022-02-07 14:08:54 +0400

    apdates Scissors (not final)

--- 5thClass/MaserovKhndirner/Problem10323/Problem10323.py
+++ 5thClass/MaserovKhndirner/Problem10323/Problem10323.py
@@ -10,40 +10,32 @@ class Problem10323(Scene):
     def construct(self):
         self.add(bounds)
 
-    # INITS
+# INITS
         numbers_font_size = 50
         labels_font_size = 50
 
         children_segment = Segment([-2.5, 2.5, 0], [-1, 2.5, 0])
         children_label = MathTex(r'\textrm{Ե.}', tex_template=armenian_tex_template, font_size=labels_font_size)
-        children_label.next_to(children_segment, LEFT)
+        children_label.next_to(children_segment[0], LEFT)
         
         men_segment_1 = children_segment.copy().shift(1.5*DOWN)
         men_label = MathTex(r'\textrm{Տ.}', tex_template=armenian_tex_template, font_size=labels_font_size)
-        men_label.next_to(men_segment_1, LEFT)
+        men_label.next_to(men_segment_1[0], LEFT)
 
-    # Animations block
-        self.play(Create(children_segment))
-        self.wait(0.25)
-        self.play(Write(children_label))
-        self.wait(0.5)
+        men_segments = VGroup(men_segment_1)
+        men_segment_2 = men_segment_1.copy().next_to(men_segment_1, buff=0)
+        men_segments.add(men_segment_2)
 
-        self.play(ReplacementTransform(children_segment.copy(), men_segment_1))
-        self.wait(0.5)
-        men_segments = MultiplySegmentRotating(self, men_segment_1, merge_segments=False, run_time=1.5)
-        self.wait(0.25)
-        self.play(Write(men_label))
-        self.wait(0.5)
-
-    # INITS
         women_segments = men_segments.copy().shift(1.5*DOWN)
+
         women_segments_extention = Segment(
-            women_segments[-1][0][0].get_start_and_end()[0], 
-            women_segments[-1][0][0].get_start_and_end()[0] + np.array([0.7, 0, 0]),
-            color=ORANGE, mathtex=MathTex(r'10', font_size=numbers_font_size)
+            women_segments[-1][0][0].get_start_and_end()[1], 
+            women_segments[-1][0][0].get_start_and_end()[1] + np.array([0.7, 0, 0]),
+            color=PURE_GREEN, endmark_color=PURE_GREEN, mathtex=MathTex(r'10', font_size=numbers_font_size)
         )
+
         women_label = MathTex(r'\textrm{Կ.}', tex_template=armenian_tex_template, font_size=labels_font_size)
-        women_label.next_to(women_segments, LEFT)
+        women_label.next_to(women_segments[0], LEFT)
 
         brace = BraceBetweenPoints(
             women_segments_extention[0][0].get_start_and_end()[1] + np.array([0.5, -0.25, 0]),
@@ -55,19 +47,20 @@ class Problem10323(Scene):
         together_minus_extra = MathTex(r'525', r'-', r'10', r'=', r'515', font_size=numbers_font_size)
         together_minus_extra.move_to(women_segments[1], DOWN).shift(1*DOWN)
 
-        numbers = VGroup(*[MathTex(rf'{i}', color=GREEN, font_size=numbers_font_size) for i in range(1, 6)])
-        numbers.move_to([0, 2.5, 0]).set_opacity(0).shift(0.5*UP)
-        numbers[0].shift(0.5*DOWN)
-        mas = MathTex(r'\textrm{մաս}', tex_template=armenian_tex_template).next_to(numbers[0], RIGHT)
-
         segments = VGroup(children_segment, *men_segments, *women_segments)
 
         dividing_by_5 = MathTex(r'515', r':', r'5', r'=', r'103', font_size=numbers_font_size)
         dividing_by_5.next_to(together_minus_extra, DOWN).shift(0.5*DOWN)
-        dividing_by_5[2].set_color(GREEN)
+        dividing_by_5[2].set_color(DEFAULT_COUNTING_COLOR)
+
+        numbers = VGroup(*[MathTex(rf'{i}', font_size=numbers_font_size) for i in range(1, 6)])
+        numbers.set_color(DEFAULT_COUNTING_COLOR)
+        numbers.move_to(dividing_by_5[2])
+        
+        mas = MathTex(r'\textrm{մաս}', tex_template=armenian_tex_template).next_to(numbers[0], RIGHT)
 
         answer = MathTex(r'103', font_size=numbers_font_size).next_to(children_segment, UP*0.5).shift(0.25*DOWN)
-        answer_rect = SurroundingRectangle(answer, color=GREEN)
+        answer_rect = SurroundingRectangle(answer, color=PURE_GREEN)
 
         open_scissors = SVGMobject('../../../Functions/SVG_PNG_files/open_scissors.svg')
         open_scissors.set_color(WHITE).rotate(PI/10).scale(0.5)
@@ -76,9 +69,24 @@ class Problem10323(Scene):
         closed_scissors.set_color(WHITE).scale(0.13).shift(0.1*DOWN).rotate(PI/5)
 
 
+# ANIMATIONS
+    # First segment
+        self.play(Create(children_segment))
+        self.wait(0.25)
+        self.play(Write(children_label))
+        self.wait(0.5)
 
-    # ANIMATIONS
+    # Second segment
+        self.play(AnimationGroup(
+            ReplacementTransform(children_segment.copy(), men_segment_1),
+            ReplacementTransform(children_segment.copy(), men_segments[1]),
+            lag_ratio=0.1, run_time=1.5
+        ))
+        self.wait(0.25)
+        self.play(Write(men_label))
+        self.wait(0.5)
 
+    # Third segment
         self.play(ReplacementTransform(men_segments.copy(), women_segments))
         self.wait(0.5)
         self.play(Create(women_segments_extention))
@@ -86,15 +94,24 @@ class Problem10323(Scene):
         self.play(Write(women_label))
         self.wait(0.5)
 
+
         self.play(Create(brace))
         self.wait(0.5)
         self.play(Write(together_525))
         self.wait(0.5)
 
-        cut_with_scissors(self, open_scissors, closed_scissors, women_segments_extention[0][0].get_start_and_end()[0])
+    # Cut the extra segment and move right
 
-        self.play(women_segments_extention.animate().shift(0.25*RIGHT))
+        cut_with_scissors(self, open_scissors, closed_scissors, women_segments_extention[0][0].get_start_and_end()[0])
+        # self.play(women_segments_extention.animate().shift(0.25*RIGHT))
         self.wait(0.5)
+
+        # self.play(women_segments_extention.animate().shift(0.25*LEFT))
+
+        scissors = Scissors(women_segments_extention[0][0].get_start_and_end()[0])
+        scissors.cut_in(self)
+        self.play(women_segments_extention.animate().shift(0.25*RIGHT))
+        scissors.cut_out(self)
 
         self.play(ReplacementTransform(together_525.copy(), together_minus_extra[0]))
         self.play(Write(together_minus_extra[1]))
@@ -102,27 +119,32 @@ class Problem10323(Scene):
         self.play(Write(together_minus_extra[3:]))
         self.wait(0.5)
         
+        self.play(ReplacementTransform(together_minus_extra[-1].copy(), dividing_by_5[0]))
+        self.play(Write(dividing_by_5[1]))
+        self.wait(0.5)
 
         for i in range(5):
-            segments[i].set_color(GREEN)
+            segments[i].set_color(DEFAULT_COUNTING_COLOR)
             self.play(segments[i].animate(rate_func=there_and_back, run_time=0.5).scale(1.2))
             segments[i].set_color(WHITE)
 
             if i == 0:
-                self.play(numbers[i].animate().set_opacity(1), run_time=0.5)
+                self.play(ReplacementTransform(segments[i].copy().set_color(DEFAULT_COUNTING_COLOR), numbers[i]))
                 self.play(Write(mas), run_time=0.5)
             else:
                 self.play(
-                    numbers[i-1].animate().shift(0.5*DOWN).set_opacity(0),
-                    numbers[i].animate().shift(0.5*DOWN).set_opacity(1),
-                    run_time=0.5
+                    AnimationGroup(
+                        ReplacementTransform(segments[i].copy().set_color(DEFAULT_COUNTING_COLOR), numbers[i]),
+                        FadeOut(numbers[i-1]),
+                        lag_ratio=0.15
+                    )
                 )
         self.wait(0.5)
         
-        self.play(ReplacementTransform(together_minus_extra[-1].copy(), dividing_by_5[0]))
-        self.play(Write(dividing_by_5[1]))
+        self.remove(numbers[-1])
+        self.add(dividing_by_5[2])
         self.play(
-            ReplacementTransform(numbers[-1], dividing_by_5[2]),
+            # ReplacementTransform(numbers[-1], dividing_by_5[2]),
             FadeOut(mas)
         )
         self.play(Write(dividing_by_5[3:]))
@@ -134,11 +156,9 @@ class Problem10323(Scene):
         self.wait(0.5)
 
 
-
-
-
 class test(Scene):
     def construct(self):
-        
-        self.wait()
+        self.add(Segment([-4, 0, 0], [4, 0, 0], stroke_width=8))
+        self.add(Segment([-4, 0, 0], [4, 0, 0]).shift(DOWN))
+
 
